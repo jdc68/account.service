aefb5c1392d10c56086ef63a5d3f086b4dc51238
JDC <deniscernat@gmail.com>
2021-06-29 16:01:00 +0300

    first commit

---
+++ src/main/java/jdc/spring/accountwebservice/AccountWebServiceApplication.java
@@ -0,0 +1,13 @@
+package jdc.spring.accountwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jdc/spring/accountwebservice/AccountWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jdc.spring.accountwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
